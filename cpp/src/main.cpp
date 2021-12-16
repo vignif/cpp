26dd7b5c8e805548fbd9239e1338c4e60fed9484
francesco <vignif@gmail.com>
2021-12-16 16:53:11 +0100

    Add Bank and threads; NOT WORKING

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,14 +1,23 @@
 #include "main.hpp"
-#include "account.hpp"
+#include "bank.hpp"
 
 int main()
 {
     std::cout << "Hello World!"
               << "\n";
 
+    Bank B("BCC");
+
     Account A1("Ciccio", 200);
     Account A2("Bob", 100);
     Account A3("Alex", 300);
-
+    B.addAccount(&A3);
+    B.addAccount(A1);
+    B.addAccount(A2);
+    B.transfer(A1, A2, 10);
+    B.transfer(A3, A2, 20);
+    B.transfer(A2, A2, 30);
+    B.compute();
+    //B.printAccounts();
     return 0;
 }
